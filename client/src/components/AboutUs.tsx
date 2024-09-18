f581c22acc196e78f79ff000a19537d6915bb8fa
AshtonGlover <113224225+AshtonGlover@users.noreply.github.com>
2024-09-18 17:53:14 -0400

    feat: added flower graphics

--- client/src/components/AboutUs.tsx
+++ client/src/components/AboutUs.tsx
@@ -1,10 +1,37 @@
 import "../styles/AboutUs.css";
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import { useRef, useEffect, useState } from "react";
+import * as THREE from 'three';
+
 
 /**
  * React component to render the about us text. Allows for easy switching between this and the chat
  * section. 
  */
 export default function AboutUs() {
+
+  const GLTFModel: React.FC<{ url: string }> = ({ url }) => {
+    const { scene } = useGLTF(url);
+    const modelRef = useRef<THREE.Group>(null);
+  
+    useEffect(() => {
+      if (modelRef.current) {
+        modelRef.current.rotation.x = -Math.PI / 2;
+        modelRef.current.position.set(0, -4, -2);
+      }
+    }, [scene]);
+  
+    useFrame(({ clock }) => {
+      if (modelRef.current) {
+        const time = clock.getElapsedTime(); 
+        modelRef.current.rotation.z = time; 
+      }
+    });
+  
+    return <primitive ref={modelRef} object={scene} />;
+  };
+
   return (
     <div aria-label="about-us" className="about-us">
       <p aria-label="about-us-text" className = "about-us-text"> 
@@ -17,6 +44,54 @@ export default function AboutUs() {
         Peer mediation provides a way to avoid bad habits when resolving conflicts and 
         allows students to talk to those will understand them the most, peers.
       </p>
+
+      <div className="model-container">
+        <Canvas>
+          <perspectiveCamera position={[0, 5, 10]} fov={100} near={0.1} far={1000} />
+          <ambientLight intensity={0.5} />
+          <spotLight 
+            position={[10, 10, 10]} 
+            angle={0.3} 
+            penumbra={0.5} 
+            intensity={100} 
+            distance={50} 
+            castShadow 
+          />
+          <spotLight 
+            position={[-10, 10, 10]} 
+            angle={0.3} 
+            penumbra={0.5} 
+            intensity={100} 
+            distance={50} 
+            castShadow 
+          />
+          <spotLight 
+            position={[10, -10, 10]} 
+            angle={0.3} 
+            penumbra={0.5} 
+            intensity={100} 
+            distance={50} 
+            castShadow 
+          />
+          <spotLight 
+            position={[10, 10, -10]} 
+            angle={0.3} 
+            penumbra={0.5} 
+            intensity={100} 
+            distance={50} 
+            castShadow 
+          />
+          <spotLight 
+            position={[0, -6, 0]}  // Positioned directly below the model
+            angle={1} 
+            penumbra={0.5} 
+            intensity={300} 
+            distance={500} 
+            castShadow 
+          />
+          <GLTFModel url="/flower.gltf"/>
+        </Canvas>
+      </div>
     </div>
   );
 }
